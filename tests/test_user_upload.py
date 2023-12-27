90b330782043de0562a638238823ac29c9bef806
Matheus Oliveira de Souza <msouza.os@hotmail.com>
2023-12-27 11:37:21 -0300

    update: checking integrity of stored dataset.

--- tests/test_user_upload.py
+++ tests/test_user_upload.py
@@ -1,7 +1,10 @@
 """
 Test user upload.
 """
+import io
 import os
+
+import pandas as pd
 
 
 def test_upload_valid_files(token, fake_new_user, valid_files, client, base_route):
@@ -20,6 +23,16 @@ def test_upload_valid_files(token, fake_new_user, valid_files, client, base_rout
             )
         # checking response
         assert response.status_code == data["status"]
+        rdata = response.json()
+        for sep in list(rdata["separator"]):
+            base_df = pd.read_csv(
+                filepath_or_buffer=data["path"], sep=sep, encoding=rdata["encoding"]
+            )
+            with io.StringIO(initial_value=rdata["file"]) as buffer:
+                resp_df = pd.read_csv(
+                    filepath_or_buffer=buffer, sep=sep, encoding=rdata["encoding"]
+                )
+            assert base_df.equals(other=resp_df)
 
 
 def test_upload_invalid_files(token, fake_new_user, invalid_files, client, base_route):
@@ -45,7 +58,7 @@ def test_delete_files(token, valid_files, client, base_route):
     Delete a list of files
     """
     # deleting files
-    for file_id, data in enumerate(iterable=valid_files):
+    for file_id, _ in enumerate(iterable=valid_files):
         response = client.delete(
             url=os.path.join(base_route, "users", "files"),
             params={"file_id": file_id + 1},
