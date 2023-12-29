939d18b493e7209fbb3cd6f502309cf4c256857d
Matheus Oliveira de Souza <msouza.os@hotmail.com>
2023-12-29 14:03:16 -0300

    create: creating file to run the main application.

---
+++ main.py
@@ -0,0 +1,23 @@
+"""
+Base script to initialize the AutoML application.
+"""
+import os
+
+
+def build_logging_folder() -> None:
+    """
+    Initializes the folder to store
+    all log files.
+    """
+    folder_name = "logs"
+    if not os.path.exists(path=folder_name):
+        os.mkdir(path=folder_name)
+
+
+if __name__=="__main__":
+    
+    # initialize folders
+    build_logging_folder()
+    
+    # imports
+    import database.config as config
