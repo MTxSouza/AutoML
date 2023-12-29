b634c85eb20fba2eac208b76787e6c442158fe7e
Matheus Oliveira de Souza <msouza.os@hotmail.com>
2023-12-29 15:53:17 -0300

    update: applying auto format to code.

--- database/config.py
+++ database/config.py
@@ -1,13 +1,13 @@
 """
 Initializes all connections to MongoDB Server.
 """
-from database.logging import logger
-
-from pymongo.errors import ServerSelectionTimeoutError, CollectionInvalid
-from motor.motor_asyncio import AsyncIOMotorClient
-
 import asyncio
 import sys
+
+from motor.motor_asyncio import AsyncIOMotorClient
+from pymongo.errors import CollectionInvalid, ServerSelectionTimeoutError
+
+from database.logging import logger
 
 ADMIN_USER = "admin"
 ADMIN_PASSWORD = 1234
@@ -18,14 +18,16 @@ CONNECTION_STRING = f"mongodb://{ADMIN_USER}:{ADMIN_PASSWORD}@{HOST}:{PORT}/"
 
 try:
     # checking connection
-    mongo_client = AsyncIOMotorClient(host=CONNECTION_STRING, serverSelectionTimeoutMS=MS_TIMEOUT)
+    mongo_client = AsyncIOMotorClient(
+        host=CONNECTION_STRING, serverSelectionTimeoutMS=MS_TIMEOUT
+    )
     logger.debug(msg="MongoDB client has been created")
-    
+
     # creating database
     db = mongo_client.get_database(name="auto_database")
     logger.debug(msg="`automl_database` database has been defined")
     collection_names = ["users", "files"]
-    
+
     # creating collections
     async def create_collections() -> None:
         for name in collection_names:
@@ -34,22 +36,23 @@ try:
         # filtering collections
         if not all(col in collection_names for col in await db.list_collection_names()):
             raise RuntimeError()
-    
+
     # checking collections
     async def check_collections() -> None:
         for name in collection_names:
             await db.validate_collection(name_or_collection=name)
             logger.debug(msg=f"`{name}` collection has been validated")
-    
+
     # run async
     async def main() -> None:
         task1 = asyncio.create_task(coro=create_collections())
         await task1
-        
+
         task2 = asyncio.create_task(coro=check_collections())
         await task2
+
     asyncio.run(main=main())
-    
+
 except ServerSelectionTimeoutError:
     logger.critical(msg="Could not connect to MongoDB Server")
     sys.exit(0)
