338e46cff91cff7d3d1a9dece17517af7933a4b3
Matheus Oliveira de Souza <msouza.os@hotmail.com>
2023-12-29 12:43:07 -0300

    update: applying auto format to code.

--- database/config.py
+++ database/config.py
@@ -5,13 +5,11 @@ from pymongo import MongoClient
 
 ADMIN_USER = "admin"
 ADMIN_PASSWORD = 1234
-HOST="localhost"
-PORT=80
+HOST = "localhost"
+PORT = 80
 CONNECTION_STRING = f"mongodb://{ADMIN_USER}:{ADMIN_PASSWORD}@{HOST}:{PORT}/"
 
-mongo_client = MongoClient(
-    host=CONNECTION_STRING
-)
+mongo_client = MongoClient(host=CONNECTION_STRING)
 
 # creating database
 db = mongo_client["automl_database"]
